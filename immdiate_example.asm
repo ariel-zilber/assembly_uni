c09adf50cf48e585c77fbcef1e24e96deb7b28b1
arikzilHuji <ariel.zilbershtey@mail.huji.ac.il>
2019-04-30 08:58:18 +0300

    first commit

--- immdiate_example.asm
+++ immdiate_example.asm
@@ -1,6 +1,3 @@
-##################
-##example program#
-##################
 	.text
 	# Declare main as a global function
 	.globl	main
@@ -8,22 +5,28 @@
 # The label 'main' represents the starting point
 main:
 
-		# Run the print_string syscall which has code 4
+jal	print_example
+
+# Code for syscall: exit
+li	$v0,10
+syscall
+
+print_example:
+	# Run the print_string syscall which has code 4
 	li	$v0,4		# Code for syscall: print_string
 	la	$a0, msg	# Pointer to string (load the address of msg)
 	syscall
 	
-# Printing out the number
-add $t0,$0,$0
-li $v0, 1
-move $a0, $t0
-syscall
- li	$v0,10		# Code for syscall: exit
-	syscall
+	# Printing out the number
+	add $t0,$0,$0
+	li $v0, 1
+	move $a0, $t0
 
+	# Return from function
+	jr $ra # Jump to addr stored in $ra
+					
 	# All memory structures are placed after the
 	# .data assembler directive
 	.data
 
 msg:	.asciiz	"Hello World!\n"
-
